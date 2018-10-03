7a632fcab98f9d3466d035142a5a134cca72314e
Tejpal Borkar <tejpalborkar@gmail.com>
2018-10-03 12:12:37 +0530

    test conflict

--- JspServletWithEmployeeApplication/src/com/assimilate/employeeapp/database/DbConnection.java
+++ JspServletWithEmployeeApplication/src/com/assimilate/employeeapp/database/DbConnection.java
@@ -6,7 +6,8 @@ import java.sql.SQLException;
 
 public class DbConnection {
 
-	public static final String URL = "jdbc:mysql://localhost:3306/employee-db";
+	public static final String URL = "jdbc:mysql://localhost:3306/employee-db_1";
+
 	public static final String DB_USER = "root";
 	public static final String PASSWORD = "root";
 
