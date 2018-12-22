34879ca272d821646f8c884f6cd4bba4f2c2d32d
Tejpal Borkar <tejpalborkar@gmail.com>
2018-12-22 11:32:53 +0530

    jstl

---
+++ LoginApplication/src/com/assimilate/loginapp/model/Employee.java
@@ -0,0 +1,48 @@
+package com.assimilate.loginapp.model;
+
+public class Employee {
+
+	private Integer employeeId;
+	private String name;
+	private String department;
+	private Integer salary;
+
+	public Integer getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(Integer employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	public Integer getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Integer salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [employeeId=" + employeeId + ", name=" + name + ", department=" + department + ", salary="
+				+ salary + "]";
+	}
+
+}
