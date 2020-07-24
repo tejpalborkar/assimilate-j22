8d3d513287280a90fae5173403856bf86f64858c
Tejpal Borkar <tejpal.borkar@coreviewsystems.com>
2020-07-24 11:44:07 +0530

    rest controller.

--- spring-boot-javafeb/src/main/java/com/assimilate/springboot/javafeb/controllers/StudentController.java
+++ spring-boot-javafeb/src/main/java/com/assimilate/springboot/javafeb/controllers/StudentController.java
@@ -1,23 +1,36 @@
 package com.assimilate.springboot.javafeb.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.assimilate.springboot.javafeb.dao.StudentDao;
+import com.assimilate.springboot.javafeb.model.Student;
+
 @RestController
 public class StudentController {
-	
 
-	@RequestMapping(value="/helloWorld")
+	@Autowired
+	StudentDao studentDao;
+
+	@RequestMapping(value = "/helloWorld")
 	public String helloWorld() {
 		System.out.println("Executing hello world");
-		return "Welcome to assimilate";
+		return "hellow";
 	}
-	
-	@RequestMapping(value="/welcome")
+
+	@RequestMapping(value = "/welcome")
 	public String welcome() {
 		System.out.println("Executing welcome");
 		return "Welcome Tejpal";
+	}
+
+	@RequestMapping(value = "/students")
+	public List<Student> students() {
+		return studentDao.findAll();
 	}
 
 }
