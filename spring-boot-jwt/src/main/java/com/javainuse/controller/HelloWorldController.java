22798fb7ccfac6beefecc020c589a06fe18d604e
Tejpal Borkar <tejpal.borkar@gmail.com>
2020-08-10 11:02:53 +0530

    jwt token based authentication

---
+++ spring-boot-jwt/src/main/java/com/javainuse/controller/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.javainuse.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	@RequestMapping({ "/hello" })
+	public String firstPage() {
+		return "Hello World";
+	}
+	
+	// JWT: Json web token
+	// sdfsdfsdfho
+
+}
